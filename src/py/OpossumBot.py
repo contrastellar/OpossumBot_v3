7dc43ecf4f31fd5fc5ddab353404ec42bfa67f4a
Gabriella Stimac <gstimac@msudenver.edu>
2024-02-14 11:39:08 -0700

    Bot runs, no DB calls, started the framework for the functions module Token must be included in run.

--- src/py/OpossumBot.py
+++ src/py/OpossumBot.py
@@ -1,6 +1,24 @@
-"""
-
-"""
+# This example requires the 'message_content' intent.
 
 import discord
+from OpossumBotFunctions import OpossumBotFunctions
 
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await OpossumBotFunctions.ping(client, message)
+
+TOKEN = open('test.token').read()
+client.run(TOKEN)
