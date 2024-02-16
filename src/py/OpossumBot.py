6d1a571531f05993d33c5067e9efbbd681892dd1
Gabriella Stimac <gstimac@msudenver.edu>
2024-02-16 14:14:16 -0700

    removed `connect.py`, moved relevant functions into `OpossumBot.py`

--- src/py/OpossumBot.py
+++ src/py/OpossumBot.py
@@ -3,6 +3,20 @@
 import discord
 from OpossumBotFunctions import OpossumBotFunctions
 
+import psycopg2
+from config import load_config
+
+def connect(config):
+    """ Connect to the PostgreSQL database server """
+    try:
+        # connecting to the PostgreSQL server
+        with psycopg2.connect(**config) as conn:
+            print('Connected to the PostgreSQL server.')
+            return conn
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
+
+
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -21,5 +35,9 @@ async def on_message(message):
         # TODO need to update this so that it returns an opossum picture
         await OpossumBotFunctions.ping(client, message)
 
-TOKEN = open('test.token').read()
+TOKEN = open('run.token').read()
 client.run(TOKEN)
+
+if __name__ == '__main__':
+    config = load_config()
+    connect(config)
