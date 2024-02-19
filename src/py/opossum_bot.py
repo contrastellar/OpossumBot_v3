83f48b7942023c73f642efde34e18c14f9b5facb
Gabriella Stimac <gstimac@msudenver.edu>
2024-02-19 11:48:58 -0700

    Renaming to conform /w pylint standards

--- src/py/opossum_bot.py
+++ src/py/opossum_bot.py
@@ -1,10 +1,12 @@
-# This example requires the 'message_content' intent.
+"""
+
+"""
 
 import discord
-from OpossumBotFunctions import OpossumBotFunctions
-
 import psycopg2
 from config import load_config
+from opossum_bot_functions import opossum_bot_functions
+
 
 def connect(config):
     """ Connect to the PostgreSQL database server """
@@ -24,19 +26,26 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
+    """
+    Print to stdout what user is being connected to (useful for debugging)
+    """
     print(f'We have logged in as {client.user}')
 
 @client.event
 async def on_message(message):
+    """
+    Handler for all on_message events.
+    """
     if message.author == client.user:
         return
 
     if message.content.startswith('!possum'):
-        # TODO need to update this so that it returns an opossum picture
-        await OpossumBotFunctions.ping(client, message)
+        
+        # TODO need to change the call here so that it sends an opossum
+        await opossum_bot_functions.ping(client, message)
 
-TOKEN = open('run.token').read()
-client.run(TOKEN)
+with open('run.token', encoding="utf-8").read() as TOKEN:
+    client.run(TOKEN)
 
 if __name__ == '__main__':
     config = load_config()
--- src/py/opossum_bot_functions/opossum_bot_functions.py
+++ src/py/opossum_bot_functions/opossum_bot_functions.py
@@ -1,5 +1,6 @@
 import psycopg2
+import discord
 
-async def ping(client, message):
+async def ping(client: discord.Client, message: any):
     await message.channel.send('This is a test message!')
     return
