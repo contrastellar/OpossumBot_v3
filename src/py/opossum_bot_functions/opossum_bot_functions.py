d59b3ae78a302d326d75ddd3e20f2b1f57223e0f
Gabriella Stimac <gstimac@msudenver.edu>
2024-02-22 11:27:02 -0700

    Expanded framework for `return_opossum()`, now successfully returns a query from the psql database on `opossum.contrastellar.com`

--- src/py/opossum_bot_functions/opossum_bot_functions.py
+++ src/py/opossum_bot_functions/opossum_bot_functions.py
@@ -13,9 +13,17 @@ async def ping(client: discord.Client, message: any):
     await message.channel.send('This is a test message!')
     return
 
-def return_opossum(client: discord.Client, message: any, ID: int) -> Image:
+async def return_opossum(conn: psycopg2.connect, client: discord.Client, message: any, ID: int) -> Image:
     """
     Function that returns the specified opossum from the
     database
     """
+    # At invokation for this funtion, the `conn` parameter is
+    # a working psycopg2 connection.
+    cur = conn.cursor()
+    # Dummy execution
+    cur.execute("SELECT img FROM opossumbot WHERE ID="+str(ID))
+    print(cur.fetchall())
+    #image = discord.File(cur.fetchall())
+    #message.channel.send(image)
     return
