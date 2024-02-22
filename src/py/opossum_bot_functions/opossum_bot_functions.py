4d9959da75301adcddf074d368a2a02f795bd850
Gabriella Stimac <gstimac@msudenver.edu>
2024-02-22 11:44:20 -0700

    Expanded `add_opossum` so that it now (in theory) adds an opossum to the table

--- src/py/opossum_bot_functions/opossum_bot_functions.py
+++ src/py/opossum_bot_functions/opossum_bot_functions.py
@@ -27,6 +27,24 @@ async def return_opossum(conn: psycopg2.connect, client: discord.Client, message
     #image = discord.File(cur.fetchall())
     #message.channel.send(image)
     return
-async def add_opossum(conn: psycopg2.connect, client: discord.Client, message: any) -> None:
 
+async def add_opossum(conn: psycopg2.connect, client: discord.Client, message: any, image: bytes) -> None:
+    """
+    Function that adds a new opossum to the database
+    The image is handled in this function.
+    """
+    # Things that need to be done to add a new opossum
+    cur = conn.cursor()
+
+    # image needs to be sent as a discord.File object, will make things easier in return_opossum
+    discord_file = discord.File(image)
+
+    # Because the image has already been made into a discord.File object,
+    # we can insert the bytes into the database.
+    cur.execute("INSERT INTO opossumbot (img) VALUES (%s)", (discord_file.fp.read()))
+
+    conn.commit()
+
+    # It's important that the bot is aware of who it is that is sending the image
+    # So that must be handled in the bot, rather than in this function.
     return
